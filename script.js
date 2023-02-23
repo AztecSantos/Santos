563a034bf4642a9aadcea1a058af45eea229113f
Santos Thakur <124021677+AztecSantos@users.noreply.github.com>
2023-02-23 17:36:25 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Student", "Learner", "Graphic Designer", "Web Developer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 900,
+    loop: true,
+  });
